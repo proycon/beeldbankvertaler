aa9d58a026020ee86a64e3c86340ec57b4691266
Maarten van Gompel <proycon@anaproy.nl>
2017-04-25 11:02:03 +0200

    initial version

---
+++ pipeline.py
@@ -0,0 +1,68 @@
+#!/usr/bin/env python3
+
+# Beeldbank vertaler pipeline
+#  Maarten van Gompel
+#  GPLv3
+
+import sys
+import os
+import json
+import argparse
+import ucto
+
+def annotations(data, quiet=False):
+    for annotation in data['annotations']:
+        annotation['caption'] = annotation['caption'].replace('\r','')
+        if '\n' in annotation['caption'].strip():
+            if not quiet: print("WARNING: Skipping caption containing newline: " + repr(annotation), file=sys.stderr)
+        elif not annotation['caption'].strip():
+            if not quiet: print("WARNING: Missing/empty caption: " + repr(annotation), file=sys.stderr)
+        else:
+            yield annotation
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description="", formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    #parser.add_argument('--storeconst',dest='settype',help="", action='store_const',const='somevalue')
+    parser.add_argument('--mosesini', type=str,help="Path to moses.ini", action='store',default="",required=False)
+    parser.add_argument('--threads',type=int,help="Threads", action='store',default=1,required=False)
+    parser.add_argument('inputfiles', nargs='+', help='Input files')
+    args = parser.parse_args()
+
+    for jsonfilename in args.inputfiles:
+        basename = os.path.basename(jsonfilename).replace('.json','')
+        print("Loading " + basename + " ...",file=sys.stderr)
+        data = json.load(open(jsonfilename,'rb'))
+
+        if not os.path.exists(basename+'.txt') or not os.path.exists(basename+'.idx'):
+            print("Extracting data from " + basename + " ...",file=sys.stderr)
+            with open(basename+'.txt','w',encoding='utf-8') as sentenceoutput, open(basename+'.idx','w') as indexfile:
+                for annotation in annotations(data):
+                    print(annotation['caption'].strip(), file=sentenceoutput)
+                    print(annotation['id'], file=indexfile)
+
+        if not os.path.exists(basename+'.tok'):
+            print("Tokenizing data for " + basename + " ...",file=sys.stderr)
+            tokenizer = ucto.Tokenizer('tokconfig-eng', sentenceperlineinput=True, sentenceperlineoutput=True,sentencedetection=False, paragraphdetection=False)
+            tokenizer.tokenize(basename + '.txt', basename + '.tok')
+
+        if not os.path.exists(basename+'.out'):
+            print("Translating " + basename + " ...",file=sys.stderr)
+            os.system("moses --threads " + str(args.threads) + " -f " + args.mosesini + " < " + basename +'.tok > ' + basename + '.out')
+
+        if not os.path.exists(basename + '.translated.json'):
+            print("Consolidating final output for " + basename + " ...",file=sys.stderr)
+            with open(basename+'.out','r',encoding='utf-8') as outputfile, open(basename+'.idx','r') as indexfile:
+                translations = {}
+                for outline, idline in zip(outputfile.readlines(), indexfile.readlines()):
+                    id = idline.strip()
+                    translations[id] = outline
+
+                for annotation in annotations(data, quiet=True):
+                    if annotation['id'] in translations:
+                        annotation['caption_nl'] = translations[annotation['id']]
+                    else:
+                        print("WARNING: No translation for " + annotation['id'] + " !!",file=sys.stderr)
+
+            with open(basename + '.translated.json','wb') as jsonout:
+                json.dump(data, jsonout)
